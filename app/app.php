44500602e185fab2766d479956104e490402d067
Mike Chastain & william swanson <student@epicodus.com>
2015-08-05 11:01:25 -0700

    Continued Dev on To Do List

--- app/app.php
+++ app/app.php
@@ -4,24 +4,56 @@
   require_once __DIR__."/../vendor/autoload.php";
   require_once __DIR__."/../src/Task.php";
 
+  session_start();
+  if (empty($_SESSION['list_of_tasks'])){
+          $_SESSION['list_of_tasks'] = array();
+
+  }
+
+
   $app = new Silex\Application();
 
   $app->get("/",function(){
-      $test_task = new Task("Get fairies.");
-      $another_test_task = new Task("Collect Triforce.");
-      $third_task = new Task("Defeat Ganon.");
 
-      $list_of_tasks = array ($test_task, $another_test_task, $third_task);
+
+
+
       $output = "";
 
-      foreach ($list_of_tasks as $task) {
+      foreach (Task::getAll() as $task) {
           $output = $output . "<p>" . $task->getDescription() . "</p>";
       }
+
+      $output .= "</ul>
+        <form action='/tasks' method='post'>
+          <label for='description'>Task Descripton</label>
+          <input id='description' name='description' type='text'>
+
+          <button type='submit'>Add task</button>
+        </form>
+        ";
+
 
 
     return $output;
 
   });
+
+
+  $app->post("/tasks",function(){
+
+    $task = new Task($_POST['description']);
+    $task->save();
+    return "
+        <h1>You created a task!</h1>
+        <p>" . $task->getDescription() . "</p>
+        <p><a href='/'>View your list of things to do.</a></p>
+    ";
+
+
+
+  })
+
 
 return $app;
 
